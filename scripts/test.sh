4a682d42bf526bd16154a5f8477235425c08e38c
Kazuhito Osabe <kzosabe@wip.place>
2022-02-19 19:56:30 +0900

    Fix CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -eu
 cd "$( dirname "$0" )"/..
-poetry run pytest tests
-poetry run black switchbot_client_app tests --check --diff
-poetry run isort switchbot_client_app tests --check --diff
-poetry run pylint switchbot_client_app tests
-poetry run mypy --install-types --non-interactive switchbot_client_app
-poetry run mypy switchbot_client_app
+poetry run tox
