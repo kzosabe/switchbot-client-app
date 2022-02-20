d47dd19d33012668fbbbaa3cbcd6ed7019114c46
Kazuhito Osabe <kzosabe@wip.place>
2022-02-20 10:26:01 +0900

    Add docs for developers

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -eux
+# In Mac you may have to reinstall python with PYTHON_CONFIGURE_OPTS like below to run this script
+# PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install {python-version}
 cd "$( dirname "$0" )"/..
-poetry run pyinstaller switchbot_client_app/main.py --onefile --noconsole
+CFLAGS=$(python-config --include) poetry run pyinstaller --onefile --name="switchbot-client-app" --windowed --noconsole switchbot_client_app/main.py
